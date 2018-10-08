79d188963a0085342f80b64b9e8484eb5dfb176a
Riccardo Righi <rr9379@gmail.com>
2018-10-08 10:21:59 +0200

    Closes #5

--- bean-processors/src/test/java/com/holonplatform/jpa/test/TestBeanPropertySetPostProcessors.java
+++ bean-processors/src/test/java/com/holonplatform/jpa/test/TestBeanPropertySetPostProcessors.java
@@ -15,10 +15,10 @@
  */
 package com.holonplatform.jpa.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import com.holonplatform.core.beans.BeanIntrospector;
 import com.holonplatform.core.beans.BeanPropertySet;
